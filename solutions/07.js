039ff45f62bef69a8074b5062b1582126c683f2d
mkrusic <mkrusic@neogov.net>
2017-12-07 13:57:43 +0100

    Day 7, part 2

--- solutions/07.js
+++ solutions/07.js
@@ -1,5 +1,5 @@
 const parseInput = (input) => {
-	return input
+	var parsed = input
 		.trim()
 		.replace(/\t/g, '')
 		.replace(/ \(/g, '|')
@@ -8,26 +8,15 @@ const parseInput = (input) => {
 		.replace(/, /g, '|')
 		.split(/\n/)
 		.map((item) => item.split('|'));
-}
-
-const getParent = (objects, child) => {
-	for (var i = 0; i < objects.length; i++) {
-		if (objects[i].children.indexOf(child) > -1)
-			return getParent(objects, objects[i].name);
-	}
-	return child;
-}
-
-const solution = (part1, input) => {
-	var head = '';
-	var parsed = parseInput(input);
 
 	var objects = [], object;
 	for (var i = 0; i < parsed.length; i++) {
 		object = {
 			name: parsed[i][0],
 			weight: +parsed[i][1],
-			children: []
+			children: [],
+			childrenWeights: [],
+			totalChildrenWeights: 0
 		}
 		if (parsed[i].length > 2) {
 			for (var j = 2; j < parsed[i].length; j++) {
@@ -36,13 +25,81 @@ const solution = (part1, input) => {
 		}
 		objects.push(object);
 	}
+	return objects;
+};
 
-	var head = getParent(objects, objects[0].name);
-	return head;
+const getHead = (objects, currentIx) => {
+	for (var i = 0; i < objects.length; i++) {
+		if (objects[i].children.indexOf(objects[currentIx].name) > -1)
+			return getHead(objects, i);
+	}
+	return currentIx;
+};
+
+const getByName = (objects, name) => {
+	for (var i = 0; i < objects.length; i++) {
+		if (objects[i].name === name) {
+			return i;
+		}
+	}
+	return -1
+};
+
+const calculateWeights = (objects, currentIx) => {
+	var object = objects[currentIx];
+	if (object.children.length === 0)
+		return object.weight;
+
+	var total = 0;
+	for (var i = 0; i < object.children.length; i++) {
+		var childObjectIx = getByName(objects, object.children[i]);
+		object.childrenWeights[i] = calculateWeights(objects, childObjectIx);
+		total += object.childrenWeights[i];
+	}
+	object.totalChildrenWeights = total;
+
+	if (total % object.childrenWeights[0] !== 0) {
+	}
+
+	return total + object.weight;
+};
+
+const getUnbalancedNode = (objects, currentIx) => {
+	var object = objects[currentIx];
+	if (object.children.length === 0)
+		return true;
+
+	if (object.totalChildrenWeights % object.childrenWeights[0] !== 0) {
+		console.log(object.name, object.children);
+		console.log(object.name, object.childrenWeights);
+	}
+
+	for (var i = 0; i < object.children.length; i++) {
+		var childObjectIx = getByName(objects, object.children[i]);
+		if (!getUnbalancedNode(objects, childObjectIx))
+			return false;
+	}
+
+	return true;
 }
 
-const part_one = (input) => solution(true, input);
-const part_two = (input) => solution(false, input);
+const part_one = (input) => {
+	var head = '';
+	var objects = parseInput(input);
+	var head = objects[getHead(objects, 0)].name;
+	return head;
+};
+
+const part_two = (input) => {
+	var head = '';
+	var objects = parseInput(input);
+	var headIx = getHead(objects, 0);
+
+	calculateWeights(objects, headIx);
+	getUnbalancedNode(objects, headIx);
+
+	return head;
+};
 
 module.exports = {
 	part_one: part_one,
