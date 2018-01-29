901871a3e21cc0e69f486677beaf10f0018cb0d3
mkrusic <mkrusic@neogov.net>
2018-01-29 20:53:01 +0100

    Day 20, part 2

--- solutions/20.js
+++ solutions/20.js
@@ -14,6 +14,20 @@ const parseInput = (raw) => {
 	return parsed;
 };
 
+const particlesInSamePos = (particle1, particle2) => {
+	return (particle1.p.x === particle2.p.x && particle1.p.y === particle2.p.y && particle1.p.z === particle2.p.z);
+};
+
+const moveParticle = (particle) => {
+	particle.v.x += particle.a.x;
+	particle.v.y += particle.a.y;
+	particle.v.z += particle.a.z;
+
+	particle.p.x += particle.v.x;
+	particle.p.y += particle.v.y;
+	particle.p.z += particle.v.z;
+};
+
 const part_one = (raw) => {
 	var input = parseInput(raw);
 	var numberOfParticles = input.length;
@@ -28,7 +42,7 @@ const part_one = (raw) => {
 
 	var minimalDistance, distance;
 	var closestParticle, position;
-	for (var j = 0; j < numberOfParticles; j++) {
+	for (var j = 0; j < particles.length; j++) {
 		position = {
 			x: particles[j].v.x + (particles[j].v.x + particles[j].a.x * 1000000),
 			y: particles[j].v.y + (particles[j].v.y + particles[j].a.y * 1000000),
@@ -46,7 +60,48 @@ const part_one = (raw) => {
 };
 
 const part_two = (raw) => {
-	return 0;
+	var input = parseInput(raw);
+	var numberOfParticles = input.length;
+	var particles = [];
+	for (var i = 0; i < numberOfParticles; i++) {
+		particles.push({
+			p: 	{	x: input[i][0],	y: input[i][1],	z: input[i][2] },
+			v: 	{	x: input[i][3],	y: input[i][4],	z: input[i][5] },
+			a: 	{	x: input[i][6],	y: input[i][7],	z: input[i][8] }
+		});
+	}
+
+	for (var i = 0; i < 1000; i++) {
+		particles.forEach((particle) => {
+			moveParticle(particle);
+		});
+
+		var j = 0, foundIdentical = false;
+		while (j < particles.length) {
+			foundIdentical = false;
+			var p = 0;
+			while (p < particles.length) {
+				if (p === j) {
+					p++;
+					continue;
+				}
+
+				if (particlesInSamePos(particles[j], particles[p])) {
+					foundIdentical = true;
+					particles.splice(p, 1);
+				} else {
+					p++;
+				}
+			}
+			if (foundIdentical) {
+				particles.splice(j, 1);
+			} else {
+				j++;
+			}
+		}
+	}
+
+	return particles.length;
 };
 
 module.exports = {
